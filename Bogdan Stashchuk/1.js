522d3d1a4b0596e6872b57224d81a22554c7f82d
Saverzus <saverzus@gmail.com>
2024-02-24 10:29:09 +0300

    Users

--- Bogdan Stashchuk/1.js
+++ Bogdan Stashchuk/1.js
@@ -1,2 +1,25 @@
-console.log('123');
-123;
+document.addEventListener("DOMContentLoaded", function () {
+    const users = {
+        Saverzus: {
+            name: 'Bogdan',
+            surname: 'Trofimenko',
+            age: 28
+        },
+        Zendro: {
+            name: 'Daniil',
+            surname: 'Serov',
+            age: 25
+        },
+        ArtemDj: {
+            name: 'Artem',
+            surname: 'Kurbatov',
+            age: 27
+        }
+    };
+
+    for (let user in users) {
+        users[user].age++;
+    }
+
+    console.log(users);
+});
