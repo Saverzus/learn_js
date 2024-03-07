c4af309e2e500928328867ab2c31f227f85ce45c
Saverzus <saverzus@gmail.com>
2024-03-08 00:45:57 +0300

    1

--- Bogdan Stashchuk/test.js
+++ Bogdan Stashchuk/test.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    const person = {
+        age: 28,
+        name: 'Bogdan'
+    }
 
+    function increseAte(person) {
+        person.age = person.age + 1;
+        return person;
+    }
+
+    console.log(person);
+    console.log(increseAte(person));
 })
