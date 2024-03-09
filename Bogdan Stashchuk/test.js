be56f558f40d84fe260fc0c869b8a24788f81275
Saverzus <saverzus@gmail.com>
2024-03-10 02:10:51 +0300

    error

--- Bogdan Stashchuk/test.js
+++ Bogdan Stashchuk/test.js
@@ -1,20 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 
-    const user = {
-        name: 'Bogdan',
-        age: 17
+    const fnWithError = () => {
+        throw new Error('Ошибка');
     }
 
-    const fnWidthError = (user) => {
-        if (user.age < 18) {
-            throw new Error("Пользователю меньше 18 лет");
-        } else {
-            throw new Error("Пользователю больше 18 лет");
-        }
+    try {
+        fnWithError();
+    } catch (error) {
+        console.error(error)
+        console.log(error.message)
     }
 
-    fnWidthError(user);
+    console.log('Продолжить код');
 
 
 
