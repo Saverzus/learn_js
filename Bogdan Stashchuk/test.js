e215f022dcba79964bdc40fa2b256cc5aac9d71c
Saverzus <saverzus@gmail.com>
2024-03-09 09:11:07 +0300

    1

--- Bogdan Stashchuk/test.js
+++ Bogdan Stashchuk/test.js
@@ -1,15 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 
-    const ooquFunction = function (a, b) {
-        let c;
-        a = a + 1;
-        c = a + b;
-        return c;
-    }
-
-    console.log(ooquFunction(5, 3));
-
+    setTimeout(function () {
+        console.log('123');
+    }, 1000)
 
 
 })
