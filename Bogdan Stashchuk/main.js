83bcd8c06d839090a4ef6aee3e8c3e348d4113c6
Saverzus <saverzus@gmail.com>
2024-03-16 13:22:31 +0300

    1
    1

--- Bogdan Stashchuk/main.js
+++ Bogdan Stashchuk/main.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Сортировка
-    // function sortArray() {
-    //     const myArray = [333, 22, 33, 123, 5, 6, 7, 8];
-    //     myArray.sort((a, b) => a - b);
+    // 1 Сортировка
+    function sortArray() {
+        const myArray = [333, 22, 33, 123, 5, 6, 7, 8];
+        myArray.sort((a, b) => a - b);
 
-    //     console.log(myArray);
-    // }
+        console.log(myArray);
+    }
 
-
-    const myArray = [1, 2, 3];
-    myArray.pop();
-    console.log(myArray);
-
+    // 2 Добавить в начало массива
+    function unshifting(a) {
+        const myArray = [1, 2, 3];
+        myArray.unshift(a);
+        console.log(myArray);
+        return myArray;
+    }
 
 })
--- Bogdan Stashchuk/test.js
+++ Bogdan Stashchuk/test.js
@@ -1,18 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Сортировка
-    // function sortArray() {
-    //     const myArray = [333, 22, 33, 123, 5, 6, 7, 8];
-    //     myArray.sort((a, b) => a - b);
-
-    //     console.log(myArray);
-    // }
-
-    // Добавить в начало массива
-    function unshifting(a) {
-        const myArray = [1, 2, 3];
-        myArray.unshift(a);
-        console.log(myArray);
-        return myArray;
-    }
 
 })
