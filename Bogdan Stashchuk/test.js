7c4e847e7eb54efc88eec0b4d2e68051112f9168
Saverzus <saverzus@gmail.com>
2024-03-16 13:14:12 +0300

    1
    1

--- Bogdan Stashchuk/test.js
+++ Bogdan Stashchuk/test.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Сортировка
+    // function sortArray() {
+    //     const myArray = [333, 22, 33, 123, 5, 6, 7, 8];
+    //     myArray.sort((a, b) => a - b);
+
+    //     console.log(myArray);
+    // }
 
 
-    const myArray = [333, 22, 33, 123, 5, 6, 7, 8];
-    myArray.sort((a, b) => a - b);
-
+    const myArray = [1, 2, 3];
+    myArray.pop();
     console.log(myArray);
+
 
 })
