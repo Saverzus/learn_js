e08b91c02739f5ebb66653d3fbc1bdc4c91baffb
Saverzus <saverzus@gmail.com>
2024-03-16 23:42:45 +0300

    comments

--- Bogdan Stashchuk/main.js
+++ Bogdan Stashchuk/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // 1 Сортировка
+    // 1 Сортировка (МЕТОД МАССИВА sort)
     function sortArray() {
         const myArray = [333, 22, 33, 123, 5, 6, 7, 8];
         myArray.sort((a, b) => a - b);
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(myArray);
     }
 
-    // 2 Добавить в начало массива
+    // 2 Добавить в начало массива (МЕТОД МАССИВА unshift)
     function unshiftArray(a) {
         const myArray = [1, 2, 3];
         myArray.unshift(a);
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         return myArray;
     }
 
-    // 3 Добавить в конец массива
+    // 3 Добавить в конец массива (МЕТОД МАССИВА push)
     function pushArray(a) {
         const myArray = [1, 2, 3];
         myArray.push(a);
@@ -23,14 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
         return myArray;
     }
 
-    // 4 Удалить первый элемент в начале массива
+    // 4 Удалить первый элемент в начале массива (МЕТОД МАССИВА shift)
     function shiftArray() {
         const myArray = [1, 2, 3];
         myArray.shift();
         console.log(myArray);
     }
 
-    // 5 Умножить элементы массива в X раз (push)
+    // 5 Умножить элементы массива в X раз (МЕТОД МАССИВА push и forEach)
     function miltuplyArray(a) {
         const myArray = [1, 2, 3, 4, 5];
         const myArrayMultipled = [];
