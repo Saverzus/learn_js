ee4c82540360fb44ca09bdf75c6402dc3a844878
Saverzus <saverzus@gmail.com>
2024-03-20 00:51:37 +0300

    // 12. Пройтись циклом forEach по массиву

--- Bogdan Stashchuk/main.js
+++ Bogdan Stashchuk/main.js
@@ -131,6 +131,15 @@ document.addEventListener("DOMContentLoaded", function () {
         return year;
     }
 
+    // 12.
+    function arrayForEach() {
+        const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        array.forEach((element, id) => {
+            console.log(element, id);
+        })
+    }
+    arrayForEach();
 
 
 })
